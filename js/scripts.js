30f95b60d4f0cd6b7b0595a2c257c9e480ceb74b
Colt Thatcher <colt.thatcher303@gmail.com>
2021-10-19 13:36:20 -0700

    add more script to the js folders

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").toggle().fadeIn(5000);
+    $("#walrus-hidden").toggle().fadeOut(5000);
   });
 });
+
+
